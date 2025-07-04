43dd84b493244371f5b421b9242eb547187311cc
b3ella <goliveirapk@gmail.com>
2025-07-04 19:56:27 -0300

    feat: configures homerow mods

--- configuration.nix
+++ configuration.nix
@@ -36,14 +36,39 @@
       internalKeyboard = {
         extraDefCfg = "process-unmapped-keys yes";
         config = ''
+
           (defsrc
            caps
+           a s d f
+           j k l
+          )
+          (defvar
+            hold-time 200
+            tap-time 200
           )
           (defalias
             escctrl (tap-hold 200 200 esc lctrl)
+
+            a-mod (tap-hold $hold-time $tap-time a lmet)
+            s-mod (tap-hold $hold-time $tap-time s lsft)
+            d-mod (tap-hold $hold-time $tap-time d lctl)
+            f-mod (tap-hold $hold-time $tap-time f (multi (layer-while-held navagation) lalt))
+
+            j-mod (tap-hold $hold-time $tap-time j (multi (layer-while-held navagation) lalt))
+            k-mod (tap-hold $hold-time $tap-time k lctl)
+            l-mod (tap-hold $hold-time $tap-time l rsft)
           )
           (deflayer base
             @escctrl
+
+            @a-mod @s-mod @d-mod @f-mod
+            @j-mod @k-mod @l-mod
+          )
+          (deflayermap navagation
+            caps tab a 1 s 2 d 3 f 4
+            tab 5 q 6 w 7 e 8 r 9
+            h left j down k up l right
+            c q
           )
         '';
       };
