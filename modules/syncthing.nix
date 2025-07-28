9695fd07afcb2294361496c0969b717573e745f6
b3ella <goliveirapk@gmail.com>
2025-07-28 14:00:24 -0300

    feat: sync folders declarativly

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -6,5 +6,24 @@
     user = "bella";
     dataDir = "/home/bella";    # Default folder for new synced folders
     configDir = "/home/bella/.config/syncthing";   # Folder for Syncthing's settings and keys
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        "celular" = {
+          id = "WU4FYL4-BOITC37-F2XJHV5-KHZ6BLV-FPKRI6H-EL2YBZJ-RNTE2TX-JWR6OA5";
+        };
+      };
+      folders = {
+        "Notes" = {
+          path = "/home/bella/General";
+          devices = ["celular"];
+        };
+        "Passwords" = {
+          path = "/home/bella/Password";
+          devices = ["celular"];
+        };
+      };
+    };
   };
 }
